a80d5ec47432a5fb7a37317e53267414fae38b07
Jesiah Roche <jesiah.roche@gmail.com>
2019-04-12 12:44:00 -0400

    Fix head shrink magic

--- js/master.js
+++ js/master.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 30190e80a0bfe46145f3b3f5f7da81159d64b118
+//Hillary's header shrink magic
 $(document).on("scroll", function() {
   console.log( $(document).scrollTop() );
   if ( $(document).scrollTop() > 100){
@@ -17,7 +14,7 @@ $(document).on("scroll", function() {
   }
 
 });
-<<<<<<< HEAD
+
 
 //contact form validation
 (function() {
@@ -37,5 +34,3 @@ $(document).on("scroll", function() {
     });
   }, false);
 })();
-=======
->>>>>>> 30190e80a0bfe46145f3b3f5f7da81159d64b118
